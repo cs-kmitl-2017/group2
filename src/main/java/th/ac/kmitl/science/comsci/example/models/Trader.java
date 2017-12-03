cb53542a86794f77eb44ffbf039f5d29ded5777c
iThanathat <brain12456@hotmail.com>
2017-12-03 13:46:04 +0700

    refactor: Change Seller model to Trader and add URIID and Email attribute

--- src/main/java/th/ac/kmitl/science/comsci/example/models/Trader.java
+++ src/main/java/th/ac/kmitl/science/comsci/example/models/Trader.java
@@ -1,6 +1,6 @@
 package th.ac.kmitl.science.comsci.example.models;
 
-public class Seller {
+public class Trader {
 
     private String id;
 
@@ -9,6 +9,19 @@ public class Seller {
     private String name;
 
     private String taxId;
+
+    private String uriId;
+
+    private String email;
+
+    public Trader(String id, String globalId, String name, String taxId, String uriId, String email) {
+        setId(id);
+        setGlobalId(globalId);
+        setName(name);
+        setTaxId(taxId);
+        setUriId(uriId);
+        setEmail(email);
+    }
 
     public String getId() {
         return id;
@@ -41,5 +54,13 @@ public class Seller {
     public String getTaxId(){
         return taxId;
     }
+
+    public String getUriId() { return uriId; }
+
+    public void setUriId(String uriId) { this.uriId = uriId; }
+
+    public String getEmail() { return email; }
+
+    public void setEmail(String email) { this.email = email; }
 
 }
---
+++ src/test/java/th/ac/kmitl/science/comsci/example/models/TraderTest.java
@@ -0,0 +1,33 @@
+package th.ac.kmitl.science.comsci.example.models;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class TraderTest {
+
+    @Test
+    public void testInputSeller() {
+
+        String id = "id58050276";
+
+        String globalId = "GId58050276";
+
+        String name = "mkproduct office";
+
+        String taxId = "TId58050276";
+
+        String uriId = "www.mkproduct.com";
+
+        String email = "mkproductinfo@mkproduct.com";
+
+        Trader trader = new Trader(id, globalId, name, taxId, uriId, email);
+
+        Assert.assertEquals(trader.getId(), id);
+        Assert.assertEquals(trader.getGlobalId(), globalId);
+        Assert.assertEquals(trader.getName(), name);
+        Assert.assertEquals(trader.getTaxId(), taxId);
+        Assert.assertEquals(trader.getUriId(), uriId);
+        Assert.assertEquals(trader.getEmail(), email);
+    }
+
+}
