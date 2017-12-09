615f08e721bbca651c5003f8125529e34954d005
Thanathat Surakhup <brain12456@hotmail.com>
2017-12-09 19:15:15 +0700

    refactor: Change class name to InputTraderController

--- src/main/ETDAapp/src/java/th/ac/kmitl/science/comsci/example/controller/InputTraderController.java
+++ src/main/ETDAapp/src/java/th/ac/kmitl/science/comsci/example/controller/InputTraderController.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class InputTrader extends HttpServlet {
+public class InputTraderController extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
